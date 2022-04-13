4e6f116d1da65ad5eb97509f77c6afae1a272f93
Connor <10349@coderacademy.edu.au>
2022-04-13 15:22:13 +1000

    wrote check_phone method in methods.rb which verifies user's phone number input gathered before creating a new user class. Ensures we hathe correct format

--- methods.rb
+++ methods.rb
@@ -13,3 +13,19 @@ def verify_name(name)
     end 
     name 
 end 
+
+#checks whether the phone number is a valid mobile number
+def check_phone 
+    puts "Please enter a 10 digit mobile number beginning with 04 or 05"
+    phone_number = gets.gsub(/\s+/, "")
+    verified = false
+    while verified == false
+        if !phone_number.count("^0-9").zero? || phone_number.empty? || phone_number.size != 10 || !phone_number.start_with?("04", "05")  
+            puts "Please enter a valid 10 digit mobile phone number beginning with 04 or 05"
+            phone_number = gets.gsub(/\s+/, "")
+        else
+            verified = true
+        end
+    end
+    phone_number
+end
