b1bc09a17901fa02a164fe482ce7ad260f182dbd
Connor <10349@coderacademy.edu.au>
2022-04-18 11:37:28 +1000

    created place_bet method. So far this breaks out of method if account_balance is 0, or if fight_card has no fights in it. Otherwise it creates an array of fight options and uses TTY prompt to allow users to select a fight they want to bet on. Then it creates a fight hash and matches the fight_select to the fight array in . Then it generates a hash with relevant details (e.g each fighter object, the index of fight in  (so we can delete after the fight) and the odds of winning (which we call the generate_odds method to set).

--- methods.rb
+++ methods.rb
@@ -35,3 +35,33 @@ def generate_odds(range, fighter_score)
         return (range / fighter_score).round(2)  
     end 
 end 
+
+def place_bet 
+
+    if $user.account_balance == 0 
+        puts "You have no credit on your account. Please make a deposit to place a bet." 
+    elsif $fight_card.size == 0 
+        puts "There are no upcoming fights to place bets on."
+    else  
+        fight_options = []
+        $fight_card.each do |i| 
+            fight_options.push("#{i[0].full_name} VS #{i[1].full_name}")  
+
+        prompt = TTY::Prompt.new
+        fight_select = prompt.select("Which upcoming fight would you like to bet on?", [fight_options, "Back"])
+        
+        fight =  {} 
+        counter = 0 
+        while counter < $fight_card.size      
+            if fight_select.include? $fight_card[counter][0].full_name
+                fight[:fight_card_index] = counter 
+                fight[:fighter_1] = $fight_card[counter][0]
+                fight[:fighter_2] = $fight_card[counter][1]
+                range = fight[:fighter_1].fighter_score + fight[:fighter_2].fighter_score
+                fight[:fighter_1_odds] = generate_odds(range, fight[:fighter_1].fighter_score)  
+                fight[:fighter_2_odds] = generate_odds(range, fight[:fighter_2].fighter_score) 
+            end 
+            counter += 1 
+        end 
+    end         
+end 
