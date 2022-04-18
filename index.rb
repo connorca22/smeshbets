31157d80ec354d2729852b2d1e565a43751da3bb
Connor <10349@coderacademy.edu.au>
2022-04-18 11:12:44 +1000

    created a tty-font object, and used the .write command to welcome user to the application on launch

--- index.rb
+++ index.rb
@@ -1,3 +1,8 @@
 require_relative "classes.rb"
 require_relative "methods.rb"
 require "tty-font"
+
+#writes app name in stylised writing 
+font = TTY::Font.new(:doom)
+puts font.write("WELCOME TO")
+puts font.write("SMESH BETS")
