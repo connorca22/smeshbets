134feaf0368f48b0b9e897d8b14896904ff2699b
Connor <10349@coderacademy.edu.au>
2022-04-18 11:15:34 +1000

    updated index.rb. created a tty prompt object and used the .ask method to request users name and phone number. Checked these values using name and phone checking methods in methods.rb. Once verified we create a new user using the User class, and store it in a global variable

--- index.rb
+++ index.rb
@@ -6,3 +6,15 @@ require "tty-font"
 font = TTY::Font.new(:doom)
 puts font.write("WELCOME TO")
 puts font.write("SMESH BETS")
+
+#requests user data then creates new user from User class
+prompt = TTY::Prompt.new
+puts "Please enter your details to register an account"
+first_name = prompt.ask("What is your first name?", required: true)
+first_name = verify_name(first_name)
+last_name = prompt.ask("What is your last name?", required: true)
+last_name = verify_name(last_name)
+phone_number = prompt.ask("Please enter a 10 digit mobile number beginning with 04 or 05", required: true).gsub(/\s+/, "")
+phone_number = check_phone(phone_number)
+
+$user = User.new(first_name, last_name, phone_number)
