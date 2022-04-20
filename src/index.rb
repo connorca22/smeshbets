93013ec70d091a3c2dbc1d277bd9332cf636d581
Connor <10349@coderacademy.edu.au>
2022-04-20 23:30:20 +1000

    Added functionality in index.rb  that pulls in command line arguments (first & last name & phone number) and bypasses the need to enter them in the app before creating user class. Only works if 3 are passed in. Still checks ARGV values are correct format

--- src/index.rb
+++ src/index.rb
@@ -7,17 +7,32 @@ font = TTY::Font.new(:doom)
 puts font.write("WELCOME TO")
 puts font.write("SMESH BETS")
 
-#requests user data then creates new user from User class
+#create tty prompt object 
 prompt = TTY::Prompt.new
-puts "Please enter your details to register an account"
-first_name = prompt.ask("What is your first name?", required: true)
-first_name = verify_name(first_name)
-last_name = prompt.ask("What is your last name?", required: true)
-last_name = verify_name(last_name)
-phone_number = prompt.ask("Please enter a 10 digit mobile number beginning with 04 or 05", required: true).gsub(/\s+/, "")
-phone_number = check_phone(phone_number)
+
+#ARGV tests
+if ARGV.size == 3 
+    first_name = ARGV[0] if ARGV[0]
+    last_name = ARGV[1] if ARGV[1]
+    phone_number = ARGV[2] if ARGV[2]
+    first_name = verify_name(first_name)
+    last_name = verify_name(last_name)
+    phone_number = check_phone(phone_number)
+else 
+#requests user data then creates new user from User class
+    puts "Please enter your details to register an account"
+    first_name = prompt.ask("What is your first name?", required: true)
+    first_name = verify_name(first_name)
+    last_name = prompt.ask("What is your last name?", required: true)
+    last_name = verify_name(last_name)
+    phone_number = prompt.ask("Please enter a 10 digit mobile number beginning with 04 or 05", required: true).gsub(/\s+/, "")
+    phone_number = check_phone(phone_number)
+end 
 
 $user = User.new(first_name, last_name, phone_number)
+
+puts "Welcome #{$user.first_name}!"
+sleep(2)
 
 #Creates all fighters 
 $fight_card = [[Fighter.new, Fighter.new], [Fighter.new, Fighter.new], [Fighter.new, Fighter.new], [Fighter.new, Fighter.new], [Fighter.new, Fighter.new]]
