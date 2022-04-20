92f12aaf45f7274ae80bd8dcfa8c8b31474251d1
Connor <10349@coderacademy.edu.au>
2022-04-20 21:44:41 +1000

    Set up rspec, and added spec folder containing spec files that test methods in methods.rb and classes.rb

---
+++ spec/classes_spec.rb
@@ -0,0 +1,22 @@
+require_relative "../classes.rb"
+
+describe "Fighter class: set_fighter_score method" do 
+    let(:fighter) {Fighter.new}
+    it "checks that fighter_score variable is set to a floating point number once method is called" do
+        expect(fighter.set_fighter_score).to be_a_kind_of(Float)
+    end 
+
+    it "checks that fighter score is within the possible range (9.0-30.0)" do
+        expect(fighter.set_fighter_score).to be >= 9 
+        expect(fighter.set_fighter_score).to be <= 30 
+    end 
+end 
+
+describe "User class: check_card method" do 
+    let(:user) {User.new('first', 'last', '0400000000')}
+    it "checks whether check_card method only proceeds if relevant conditions are met" do
+        expect(user.check_card('123', 3)).not_to match(/[^0-9]/) #checks if number string only contains digits. 
+        expect(user.check_card('123', 3)).not_to be_empty #checks whether string is empty. 
+        expect(user.check_card('123', 3).length).to be(3) #checks if card number length is equal to digits provided.
+    end 
+end 
---
+++ spec/methods_spec.rb
@@ -0,0 +1,34 @@
+require_relative "../methods.rb"
+require_relative "../classes.rb"
+
+describe "verify_name" do 
+    it "checks that the verify_name method is only returning strings that contain letters, spaces, hyphens and apostrophes" do
+        expect(verify_name("Al' Laeson-li")).not_to match(/[^a-zA-z' \-]/)
+    end 
+end 
+
+describe "check_phone" do 
+    it "checks that chec_phone method is only returning a string with numbers contained inside." do
+        expect(check_phone('0407264036')).not_to match(/[^0-9]/)
+    end 
+end 
+
+describe "generate odds method" do 
+    it "takes range and fighter_score returns the odds as a floating point number" do 
+        expect(generate_odds(35.0, 16.0)).to be_a_kind_of(Float)
+    end 
+
+    it "the range and the fighter score should never produce results outside of the minimum odds (1.10) and the highest potential odds (4.33)" do 
+        expect(generate_odds(39.0, 30.0)).to be_between(1.10, 4.33)
+        expect(generate_odds(39.0, 9.0)).to be_between(1.10, 4.33)
+    end 
+end 
+
+
+describe "fighting method" do 
+    let(:fighter_1) {Fighter.new} 
+    let(:fighter_2) {Fighter.new} 
+    it "fighting method should take fighter objects and return one of them as the winner" do
+        expect(fighting(fighter_1, fighter_2, fighter_1)).to be(fighter_1).or be(fighter_2) 
+    end 
+end 
